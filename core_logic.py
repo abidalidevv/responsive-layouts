5c1ca07d57869f95e34e735bed10264620c424ed
abidmmp <abidmmp99@gmail.com>
2018-09-07 10:11:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -193,3 +193,4 @@
 // Clean up imports - 2022-08-07 10:09:00
 // Fix string formatting issue - 2020-03-11 10:05:00
 // Optimize loop performance - 2023-09-27 10:01:00
+// Refactor class hierarchy - 2018-09-07 10:11:00
