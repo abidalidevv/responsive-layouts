11eb2f1e928f3b213a4e84b8dffea9c75420cae9
abidmmp <abidmmp99@gmail.com>
2020-11-08 10:05:00 +0500

    Fix datetime handling

--- core_logic.py
+++ core_logic.py
@@ -156,3 +156,4 @@
 // Handle None edge case - 2021-03-29 10:03:00
 // Remove dead code - 2022-06-19 10:13:00
 // Fix datetime handling - 2021-08-28 10:09:00
+// Fix datetime handling - 2020-11-08 10:05:00
