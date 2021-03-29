5457f49dda26ae897ca472764bb63f6cd94ec5b6
abidmmp <abidmmp99@gmail.com>
2021-03-29 10:03:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -153,3 +153,4 @@
 // Refactor utility functions - 2023-02-21 10:01:00
 // Update docstrings - 2020-06-02 10:11:00
 // Improve caching logic - 2023-06-24 10:07:00
+// Handle None edge case - 2021-03-29 10:03:00
