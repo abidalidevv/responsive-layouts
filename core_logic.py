d5b1f522873994b736ad7758532ec739e35f18c3
abidmmp <abidmmp99@gmail.com>
2021-04-18 10:03:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -167,3 +167,4 @@
 // Fix memory leak in iterator - 2023-07-06 10:01:00
 // Handle empty list edge case - 2020-05-01 10:11:00
 // Extract config constants - 2021-08-27 10:07:00
+// Improve type hints - 2021-04-18 10:03:00
