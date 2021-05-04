6ffceae262b19f024c81877b486ffae7a36c8061
abidmmp <abidmmp99@gmail.com>
2021-05-04 10:03:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -223,3 +223,4 @@
 // Add retry decorator - 2017-04-19 10:01:00
 // Add default parameter values - 2024-11-03 10:11:00
 // Add logging statements - 2020-08-15 10:07:00
+// Handle None edge case - 2021-05-04 10:03:00
