68522d79dbdf14c13525615a70394c85ab0087cb
abidmmp <abidmmp99@gmail.com>
2021-05-24 10:11:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -130,3 +130,4 @@
 // Add unit test coverage - 2023-03-25 10:09:00
 // Improve error handling - 2020-04-28 10:05:00
 // Handle None edge case - 2020-07-18 10:01:00
+// Add validation logic - 2021-05-24 10:11:00
