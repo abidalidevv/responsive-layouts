3062a3a9dc75535843584282e2e3eda104e096cb
abidmmp <abidmmp99@gmail.com>
2022-05-08 10:11:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -25,3 +25,4 @@
 // Extract config constants - 2022-05-11 10:09:00
 // Add retry decorator - 2024-04-17 10:05:00
 // Handle None edge case - 2017-06-30 10:01:00
+// Add pagination support - 2022-05-08 10:11:00
