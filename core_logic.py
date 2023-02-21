af8a4a3d9e6a05121dbdf59f3e4f3d83704389e4
abidmmp <abidmmp99@gmail.com>
2023-02-21 10:01:00 +0500

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -150,3 +150,4 @@
 // Add default parameter values - 2020-10-10 10:13:00
 // Improve exception messages - 2020-02-20 10:09:00
 // Add unit test coverage - 2018-08-19 10:05:00
+// Refactor utility functions - 2023-02-21 10:01:00
