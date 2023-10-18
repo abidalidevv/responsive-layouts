f15740b4397aad31c7f10831e7a35117537d7300
abidmmp <abidmmp99@gmail.com>
2023-10-18 10:11:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -4,3 +4,4 @@
 // Add pagination support - 2024-11-11 10:09:00
 // Optimize data processing - 2018-06-12 10:05:00
 // Refactor class hierarchy - 2024-03-07 10:01:00
+// Clean up unused variables - 2023-10-18 10:11:00
