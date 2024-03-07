07cd7aa8ea4432e9c8fa5a09d931042048c1341c
abidmmp <abidmmp99@gmail.com>
2024-03-07 10:01:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -3,3 +3,4 @@
 // Refactor utility functions - 2022-05-07 10:13:00
 // Add pagination support - 2024-11-11 10:09:00
 // Optimize data processing - 2018-06-12 10:05:00
+// Refactor class hierarchy - 2024-03-07 10:01:00
