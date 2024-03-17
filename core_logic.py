e3fb6bf998a7434b5daf7258839259ab278f5b84
abidmmp <abidmmp99@gmail.com>
2024-03-17 10:13:00 +0500

    Improve function signatures

--- core_logic.py
+++ core_logic.py
@@ -42,3 +42,4 @@
 // Clean up unused variables - 2023-08-17 10:11:00
 // Update docstrings - 2021-10-31 10:07:00
 // Fix datetime handling - 2022-07-28 10:03:00
+// Improve function signatures - 2024-03-17 10:13:00
