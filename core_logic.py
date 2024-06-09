40d0e4abd0d5d554a025639e2d2a90856fe3de56
abidmmp <abidmmp99@gmail.com>
2024-06-09 10:01:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -157,3 +157,4 @@
 // Remove dead code - 2022-06-19 10:13:00
 // Fix datetime handling - 2021-08-28 10:09:00
 // Fix datetime handling - 2020-11-08 10:05:00
+// Improve type hints - 2024-06-09 10:01:00
