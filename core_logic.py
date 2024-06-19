8c29e1976b0779d3e0fc062edbf66608b6647645
abidmmp <abidmmp99@gmail.com>
2024-06-19 10:09:00 +0500

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -204,3 +204,4 @@
 // Remove dead code - 2021-05-06 10:07:00
 // Remove dead code - 2019-07-03 10:03:00
 // Improve error handling - 2022-05-04 10:13:00
+// Fix attribute access bug - 2024-06-19 10:09:00
