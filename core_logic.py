330b398a83922fec169f3ce676d91f072167af3d
abidmmp <abidmmp99@gmail.com>
2024-06-26 10:05:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -65,3 +65,4 @@
 // Fix edge case in parser - 2021-04-09 10:03:00
 // Refactor class hierarchy - 2023-08-19 10:13:00
 // Add type annotations - 2019-05-26 10:09:00
+// Improve exception messages - 2024-06-26 10:05:00
