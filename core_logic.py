bf2d11ca414c53e6258dd7a199ca51d16d45a4a8
abidmmp <abidmmp99@gmail.com>
2024-11-11 10:09:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Extract config constants - 2024-07-22 10:07:00
 // Update docstrings - 2020-07-10 10:03:00
 // Refactor utility functions - 2022-05-07 10:13:00
+// Add pagination support - 2024-11-11 10:09:00
