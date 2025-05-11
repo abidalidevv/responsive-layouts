e1dcbaacf9df69b927635d36cd10100fc4de71a2
Abid Ali <abidmmp99@gmail.com>
2025-05-11 16:11:40 +0000

    Add unit test coverage

--- core_logic.py
+++ core_logic.py
@@ -298,3 +298,10 @@
 
 def safe_divide(a, b, default=0):
     return a / b if b != 0 else default
+
+def memoize(fn):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache: cache[args] = fn(*args)
+        return cache[args]
+    return wrapper
