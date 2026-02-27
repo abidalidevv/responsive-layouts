b288308de8ca82b2ab5fa08c65e669285b61ba63
Abid Ali <abidmmp99@gmail.com>
2026-02-27 11:39:23 +0000

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -492,3 +492,10 @@ def flatten(nested):
 
 def get_env(key, default=''):
     import os; return os.environ.get(key, default)
+
+def memoize(fn):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache: cache[args] = fn(*args)
+        return cache[args]
+    return wrapper
